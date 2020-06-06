5f4ee846e3170ecb53a7948adac040cd47c0842e
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-06-06 16:23:44 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 import WeatherCard from "./WeatherCard";
 import Login from "./Login.js";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Route path="/" component ={Home}/>
+      </BrowserRouter>
+
       <Login></Login>
 
       <WeatherCard />
