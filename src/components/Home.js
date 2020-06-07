d2e5422328700e864970b0c98067f38af57fb81e
staceykeating <staceyamberkk@gmail.com>
2020-06-07 12:28:51 -0700

    homepage started

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import Nav from './Nav'
+import React from "react";
+import ProfileButton from "./ProfileButton";
+import "./Home.scss";
+import Button from "@material-ui/core/Button";
 
 export default function Home() {
   return (
-    <div>
-      <Nav />
-      <h1>HOME</h1>
-    </div>
-  )
+    <>
+      <div class="home-page">
+        <div class="right-menu">
+          <Button variant="outlined">Login</Button>
+          <ProfileButton />
+          <h2>Joey</h2>
+        </div>
+        <img
+          class="header-logo"
+          src="./hatch-main-logo-dark.png"
+          alt="Main Header"
+        />
+      </div>
+      <div class="learn-section">
+        <div class="icon-with-text">
+          <h5>Step One: Do a Thing</h5>
+          <img class="globe" src="./globe.png" alt="world" />
+        </div>
+        <div class="icon-with-text">
+          <h5>Then do Another Thing</h5>
+          <img class="globe" src="./globe.png" alt="world" />
+        </div>
+        <div class="icon-with-text">
+          <h5>Also do this thing!!</h5>
+          <img class="globe" src="./globe.png" alt="world" />
+        </div>
+      </div>
+    </>
+  );
 }
