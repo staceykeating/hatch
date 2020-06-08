4cd70b86fc2706e5464c31554383d218cc968317
staceykeating <staceyamberkk@gmail.com>
2020-06-08 14:38:40 -0700

    packing list cleanup and add button functionality

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,49 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import PackingListItem from "./PackingListItem";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import Fab from "@material-ui/core/Fab";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import "./PackingList.scss";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-}));
-
 export default function PackingList() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+  const [list, setList] = useState([]);
+  const addPackingListItem = () => {
+    const newPackingListItem = {
+      id: Math.random().toString(),
+      text: "text",
+    };
+    setList([...list, newPackingListItem]);
+  };
+
   return (
-    <>
-      <Card className={classes.root}>
-        <CardContent>
-          <Typography variant="h5" component="h2">
-            Packing List
-            <AddCircleIcon />
-          </Typography>{" "}
-          <Typography variant="body2" component="p">
-            <PackingListItem />
-            <br />
-          </Typography>
-        </CardContent>
-        <CardActions></CardActions>
-      </Card>
-    </>
+    <Card>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          Packing List <AddCircleIcon onClick={addPackingListItem} />
+        </Typography>{" "}
+        <container>
+          {list.map((item) => {
+            return <PackingListItem key={item.id} text={item.text} />;
+          })}
+        </container>
+      </CardContent>
+    </Card>
   );
 }
