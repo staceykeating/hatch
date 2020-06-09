a41f5eddbdd81ab0b9ed9b019f9f3c42d52de668
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-06-09 00:01:41 -0700

    places search bar created

--- src/components/CreateTrip.js
+++ src/components/CreateTrip.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Nav from "./Nav";
 import Calendar from "./Calendar";
-import GoogleMaps from "./placesSearch";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import PlaceSearch from "./PlaceSearch"
+import axios from 'axios';
 
 function CreateTrip() {
+  const [places, setPlaces] = useState([]);
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  useEffect(() => {
+      places.forEach(place => {
+        axios.get(`/api/place_details/${place.place_id}`)
+        .then(res => {
+          console.log(`lat: ${res.data.result.geometry.location.lat} lng:${res.data.result.geometry.location.lng}`)
+        })
+      })
+  }, [isSubmitted])
+
+  
+
   return (
     <>
       <Nav />
-      <Calendar />
-      <GoogleMaps />
-    
+
+      {/* <Calendar /> */}
+      <form onSubmit={event => event.preventDefault()}>
+        <PlaceSearch 
+          setPlaces={ setPlaces }
+        />
+        <button 
+          type='submit'
+          onClick={ () => setIsSubmitted(true) }
+          >Submit</button>
+      </form>
+
     </>
   );
 }
