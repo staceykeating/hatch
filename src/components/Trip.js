d4d944fab27b81c8d4382fc3e6f0b83a697bd636
staceykeating <staceyamberkk@gmail.com>
2020-06-09 10:42:35 -0700

    add alt text to trip page

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,12 +1,12 @@
 import React from "react";
 import PackingList from "./PackingList";
 import HatchMates from "./HatchMates";
+import TripLoading from "./TripLoading";
 
 export default function Trip() {
   return (
     <>
-      <HatchMates />
-      <PackingList />
+      <TripLoading />
     </>
   );
 }
