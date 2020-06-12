a43f67e06a1f2bd15d39293b29aaddbd65539f97
staceykeating <staceyamberkk@gmail.com>
2020-06-12 16:26:21 -0700

    trip page tabs imported

--- src/components/DestinationTab.js
+++ src/components/DestinationTab.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
-import WeatherCard from './WeatherCard'
+import React, { useState } from "react";
+import WeatherCard from "./WeatherCard";
+import AddComponentButton from "./AddComponentButton";
+import ComponentCard from "./ComponentCard";
+import "./DestinationTab.scss";
 
 export default function Destination(props) {
+  const [newItem, setNewItem] = useState(false);
+
+  const addComponentBox = () => {
+    setNewItem(true);
+  };
+
+  const newInput = newItem ? <ComponentCard /> : null;
   return (
     <>
-      <WeatherCard destination={props.destination}/>
+      <WeatherCard destination={props.destination} />
       <div>You are on the {props.destination.name} page</div>
+      <div class="page-components">
+        <ComponentCard />
+        {newInput}
+        <AddComponentButton onClick={addComponentBox} />
+      </div>
     </>
-  )
+  );
 }
--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,31 +1,25 @@
 import React, { useState, useEffect } from "react";
+import useVisualMode from "../hooks/useVisualMode.js";
+import axios from "axios";
 import Nav from "./Nav";
 import Map from "./Map";
 import PackingList from "./PackingList";
 import HatchMates from "./HatchMates";
 import WeatherCard from "./WeatherCard";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import TripDates from "./TripDates";
 import TripPageTitle from "./TripPageTitle";
-import TripLoading from './TripLoading';
-import Cookies from 'js-cookie';
+import TripLoading from "./TripLoading";
+import TripTabs from "./TripTabs";
+import Cookies from "js-cookie";
+import DestinationTab from "./DestinationTab";
 import { Redirect } from "react-router-dom";
-
-
-
-import axios from "axios";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-  },
-}));
+import TestPage from "./Testpage";
 
 export default function Trip(props) {
+  const [modes] = useState({ MAIN: "MAIN" });
+
+  const { mode, transition } = useVisualMode();
   const [state, setState] = useState({
     packingList: [],
     destinations: [],
@@ -35,7 +29,7 @@ export default function Trip(props) {
     title: "",
     description: "",
   });
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     axios
@@ -61,39 +55,51 @@ export default function Trip(props) {
       });
   }, [props]);
 
-  const user = Cookies.get('user');
+  const user = Cookies.get("user");
 
-  return !user 
-  ? (<Redirect to="/login" />)
-  : (!loaded 
-    ? (<TripLoading />)
-    : (
-      <>
-        <Nav />
-        {console.log("render")}
-        <div class="trip-page-column">
-          <div class="trip-row">
-            <TripPageTitle title={state.title} description={state.description} />
-            <HatchMates
-              collaborators={state.collaborators}
-              tripID={props.match.params.id}
-            />
-          </div>
-          <div class="trip-row">
-            <TripDates startDate={state.startDate} endDate={state.endDate} />
-          </div>
-          <div class="trip-row">
-            <PackingList
-              packingList={state.packingList}
-              tripID={props.match.params.id}
-            />
-            <div id="map-box">
-              <Card>
-                <Map destinations={state.destinations} />
-              </Card>
+  state.destinations.forEach((destination) => {
+    modes[destination.destination.name] = destination.destination;
+  });
+
+  return !user ? (
+    <Redirect to="/login" />
+  ) : !loaded ? (
+    <TripLoading />
+  ) : (
+    <>
+      <Nav />
+      <TripTabs destinations={state.destinations} transition={transition} />
+      {mode === "MAIN" && (
+        <>
+          <div class="trip-page-column">
+            <div class="trip-title-row">
+              <TripPageTitle
+                title={state.title}
+                description={state.description}
+              />
+              <HatchMates
+                collaborators={state.collaborators}
+                tripID={props.match.params.id}
+              />
+            </div>
+            <div class="trip-date">
+              <TripDates startDate={state.startDate} endDate={state.endDate} />
+            </div>
+            <div class="trip-row">
+              <PackingList
+                packingList={state.packingList}
+                tripID={props.match.params.id}
+              />
+              <div id="map-box">
+                <Card>
+                  <Map destinations={state.destinations} />
+                </Card>
+              </div>
             </div>
           </div>
-        </div>
-      </>
-    ))
+        </>
+      )}
+      {mode !== "MAIN" && <DestinationTab destination={modes[mode]} />}
+    </>
+  );
 }
