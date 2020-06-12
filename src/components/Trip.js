334d37895017a70bb25c407c1646949f5ea6931e
staceykeating <staceyamberkk@gmail.com>
2020-06-11 18:08:09 -0700

    working on trip page layout

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -4,9 +4,22 @@ import Map from "./Map";
 import PackingList from "./PackingList";
 import HatchMates from "./HatchMates";
 import WeatherCard from "./WeatherCard";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+
 import axios from "axios";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  },
+}));
 
 export default function Trip(props) {
+  const classes = useStyles();
   const [state, setState] = useState({
     packingList: [],
     destinations: [],
@@ -32,17 +45,28 @@ export default function Trip(props) {
     <>
       <Nav />
       {console.log("render")}
-      <div class="trip-page">
-        <WeatherCard />
-        <PackingList 
-          packingList={state.packingList} 
-          tripID={props.match.params.id}
+      <div class="trip-page-column">
+        <div class="trip-row">
+          <div class="trip-title-section">
+            <h1>Euro Trip</h1>
+            <h3>Jessica is getting another divorce!</h3>
+          </div>
+          <HatchMates
+            collaborators={state.collaborators}
+            tripID={props.match.params.id}
           />
-        <HatchMates
-          collaborators={state.collaborators}
-          tripID={props.match.params.id}
-        />
-        <Map destinations={state.destinations} />
+        </div>
+        <div class="trip-row">
+          <PackingList
+            packingList={state.packingList}
+            tripID={props.match.params.id}
+          />
+          <div id="map-box">
+            <Card>
+              <Map destinations={state.destinations} />
+            </Card>
+          </div>
+        </div>
       </div>
     </>
   );
