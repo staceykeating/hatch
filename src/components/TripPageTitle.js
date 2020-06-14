7a6ac94d6069e40b6ffd25f8d4566eb6c54b18ca
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-06-14 15:24:36 -0700

    date formatting as month day year

--- src/components/TripPageTitle.js
+++ src/components/TripPageTitle.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./TripTitlePage.scss";
+import Moment from 'react-moment';
+
 
 export default function TripPageTitle(props) {
+  const startDate = props.startDate
+  const endDate = props.endDate
   return (
     <div id="trip-title-section">
       <h1>{props.title}</h1>
       <h1>
-        {props.startDate} to {props.endDate}
+      <Moment format="MMM Do YYYY">
+        {startDate}
+       </Moment>
+        {" "}
+       to
+        {" "}
+       <Moment format="MMM Do YYYY">
+        {endDate}
+       </Moment>
       </h1>
       <h3>{props.description}</h3>
     </div>
