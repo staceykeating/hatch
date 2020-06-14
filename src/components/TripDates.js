2352b879d7870e49d818460b5310606dcb81be66
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-06-14 13:47:13 -0700

    moment

--- src/components/TripDates.js
+++ src/components/TripDates.js
@@ -1,14 +1,21 @@
 import React, {useState} from "react";
 // import Typography from "@material-ui/core/Typography";
-import moment from "moment";
+import Moment from 'react-moment';
 
 export default function TripDates(props) {
+  const [startDate, setStartDate] = useState(props.startDate)
+  const [endDate, setEndDate] = useState(props.endDate)
+
+  const tripDate = startDate
 
 
   return (
     <div>
       <h2>
-      {props.startDate} - {props.endDate}
+
+        {props.startDate} - {props.endDate}
+        
+
       </h2>
     </div>
   );
