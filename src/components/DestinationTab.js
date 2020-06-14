4dd21ea4af9acd31a227c1f90105125256e26c91
staceykeating <staceyamberkk@gmail.com>
2020-06-14 14:40:12 -0700

    reorganize

--- src/components/DestinationTab.js
+++ src/components/DestinationTab.js
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from "react";
+import useAppData from "../hooks/useAppData.js";
 import WeatherCard from "./WeatherCard";
 import AddComponentButton from "./AddComponentButton";
 import ComponentCard from "./ComponentCard";
 import AddCircleOutlinedIcon from "@material-ui/icons/AddCircleOutlined";
-
 import "./DestinationTab.scss";
 
 export default function Destination(props) {
   const [components, setComponents] = useState(props.components);
   const [newItem, setNewItem] = useState(false);
-
+  const { state, setState } = useAppData();
   const addComponentBox = () => {
     console.log("adding");
     setNewItem(true);
   };
 
-  const newInput = newItem ? <ComponentCard /> : null;
+  const newInput = newItem ? (
+    <ComponentCard
+      destination_id={props.destination.destination.id}
+      getData={props.getData}
+    />
+  ) : null;
   return (
     <>
       <WeatherCard destination={props.destination.destination} />
@@ -27,6 +32,7 @@ export default function Destination(props) {
             <ComponentCard
               component={component}
               setComponents={setComponents}
+              getData={props.getData}
               destination_id={props.destination.destination.id}
             />
           );
--- src/components/EditButton.js
+++ src/components/EditButton.js
@@ -1,15 +1,14 @@
-import React, {useState} from "react";
-import CreateTwoToneIcon from '@material-ui/icons/CreateTwoTone';
-import SearchIcon from '@material-ui/icons/Search';
-import TextFieldsIcon from '@material-ui/icons/TextFields';
-import DeleteIcon from '@material-ui/icons/Delete';
-import CloseIcon from '@material-ui/icons/Close';
-import Textbox from './Textbox';
-import PlaceSearch from './PlaceSearch';
-import axios from 'axios';
+import React, { useState } from "react";
+import useAppData from "../hooks/useAppData.js";
+import CreateTwoToneIcon from "@material-ui/icons/CreateTwoTone";
+import SearchIcon from "@material-ui/icons/Search";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import DeleteIcon from "@material-ui/icons/Delete";
+import CloseIcon from "@material-ui/icons/Close";
+import Textbox from "./Textbox";
+import PlaceSearch from "./PlaceSearch";
+import axios from "axios";
 import { Button, Typography } from "@material-ui/core";
-
-
 
 export default function EditButton(props) {
   // const [onEdit, setOnEdit] = useState(false);
@@ -18,58 +17,56 @@ export default function EditButton(props) {
   // const [onText, setOnText] = useState(false);
   const [mode, setMode] = useState("EDIT");
 
-
   const onDelete = () => {
-      axios({
-        method: "DELETE",
-        url: `/api/components/${props.component_id}`,
-        data: {
-          destination_id : props.destination_id
-        },
-      })
+    axios({
+      method: "DELETE",
+      url: `/api/components/${props.component_id}`,
+      data: {
+        destination_id: props.destination_id,
+      },
+    })
       .then((res) => {
-        props.setComponents(res.data)
-        setMode("EDIT")
-        console.log("DELETE:",res.data);
+        props.setComponents(res.data);
+        setMode("EDIT");
+        console.log("DELETE:", res.data);
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
   };
 
   const text = (
-  <div>
-    <Textbox>
-    </Textbox>
-    <CloseIcon onClick={() => setMode("EDIT")}/>
-  </div>)
+    <div>
+      <Textbox></Textbox>
+      <CloseIcon onClick={() => setMode("EDIT")} />
+    </div>
+  );
 
   const search = (
-  <div>
-    <PlaceSearch>
-    </PlaceSearch>
-    <CloseIcon onClick={() => setMode("EDIT")}/>
-  </div>)
-
+    <div>
+      <PlaceSearch></PlaceSearch>
+      <CloseIcon onClick={() => setMode("EDIT")} />
+    </div>
+  );
 
   return (
     <div>
-      {mode === "EDIT" && <CreateTwoToneIcon onClick={() => setMode("SHOW")}/>}
-      {mode === "SHOW" && <TextFieldsIcon onClick={() => setMode("TEXT")}/>}
+      {mode === "EDIT" && <CreateTwoToneIcon onClick={() => setMode("SHOW")} />}
+      {mode === "SHOW" && <TextFieldsIcon onClick={() => setMode("TEXT")} />}
       {mode === "TEXT" && text}
       {mode === "SHOW" && <SearchIcon onClick={() => setMode("SEARCH")} />}
       {mode === "SEARCH" && search}
-      {mode === "SHOW" && <DeleteIcon onClick={() => setMode("VERIFYDELETING")}/>}
-      {mode === "SHOW" && <CloseIcon onClick={() => setMode("EDIT")}/>}
-      {mode === "VERIFYDELETING" &&
-      <Typography>
-      Are you sure you want to delete?
-      <Button onClick={() => setMode("EDIT")}>No</Button>
-      <Button onClick={() => onDelete()}>Yes</Button>
-      </Typography>}
-
+      {mode === "SHOW" && (
+        <DeleteIcon onClick={() => setMode("VERIFYDELETING")} />
+      )}
+      {mode === "SHOW" && <CloseIcon onClick={() => setMode("EDIT")} />}
+      {mode === "VERIFYDELETING" && (
+        <Typography>
+          Are you sure you want to delete?
+          <Button onClick={() => setMode("EDIT")}>No</Button>
+          <Button onClick={() => onDelete()}>Yes</Button>
+        </Typography>
+      )}
     </div>
-  )
+  );
 }
-
-
