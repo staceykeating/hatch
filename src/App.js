ea32183c2343a9ea38ad26037f68cd9c2a32153c
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-06-13 17:32:49 -0700

    merge resolve

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 import Trip from "./components/Trip.js";
 import CreateTrip from "./components/CreateTrip";
-<<<<<<< HEAD
-=======
 import Testpage from "./components/Testpage";
->>>>>>> 11bfda8cc8df1b49d266b31d1291afce8efe134e
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -19,10 +16,7 @@ function App() {
         <Route path="/create-trip" component={CreateTrip} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/trip/:id" component={Trip} />
-<<<<<<< HEAD
-=======
         <Route path="/add-button" component={Testpage} />
->>>>>>> 11bfda8cc8df1b49d266b31d1291afce8efe134e
         <Route path="/" component={Home} />
       </Switch>
     </Router>
