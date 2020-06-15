f3142cb52f63ea23a47c269a082f6d3544918579
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-06-14 18:15:33 -0700

    format date and edit date css

--- src/components/TripDates.js
+++ src/components/TripDates.js
@@ -1,21 +1,14 @@
 import React, {useState} from "react";
-// import Typography from "@material-ui/core/Typography";
 import Moment from 'react-moment';
 
 export default function TripDates(props) {
-  const [startDate, setStartDate] = useState(props.startDate)
-  const [endDate, setEndDate] = useState(props.endDate)
-
-  // const tripDate = Moment(startDate, "MM/DD/YYYY");
 
 
   return (
     <div>
       <h2>
-
-      <Moment format="D MMM YYYY"> 
-        </Moment>
-
+      <Moment format="D MMM YYYY">
+      </Moment>
       </h2>
     </div>
   );
