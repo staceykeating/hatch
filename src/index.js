89cd85758875fd879747b6c68a143f5f09cfb24d
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-06-16 15:29:29 -0700

    added deployed server api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./components/serviceWorker";
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
