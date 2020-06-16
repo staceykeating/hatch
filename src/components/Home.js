62e6cb9a264311292c475446bfb157f915f8a221
staceykeating <staceyamberkk@gmail.com>
2020-06-15 22:05:23 -0700

    adding home options

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,10 @@ import HomeProfileButton from "./HomeProfileButton";
 import "./Home.scss";
 import mainLogo from "./images/hatch-main-logo-dark.png";
 import giffy from "./images/screen-recording.gif";
+import happyFriends from "./images/happy-friends.png";
+import searchyWorld from "./images/searchy-world.png";
+import thinkyHead from "./images/thinky-head.png";
+import hatchMates from "./images/hatch-mates-69.png";
 
 export default function Home() {
   return (
@@ -14,15 +18,15 @@ export default function Home() {
       <div class="learn-section">
         <div class="icon-with-text">
           <h5>Step One: Do a Thing</h5>
-          <img class="globe" src={giffy} alt="world" />
+          <img class="globe" src={thinkyHead} alt="world" />
         </div>
         <div class="icon-with-text">
-          <h5>Then do Another Thing</h5>git
-          <img class="globe" src="./globe.png" alt="world" />
+          <h5>Then do Another Thing</h5>
+          <img class="mates" src={hatchMates} alt="world" />
         </div>
         <div class="icon-with-text">
           <h5>Also do this thing!!</h5>
-          <img class="globe" src="./globe.png" alt="world" />
+          <img class="globe" src={searchyWorld} alt="world" />
         </div>
       </div>
     </>
